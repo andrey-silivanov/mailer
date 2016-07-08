1325f9b442f273416c13c957774530ebbb5491fc
werder33 <andreywow90@hmail.com>
2016-07-09 00:02:06 +0300

    add controller , model , view

---
+++ app/Http/Controllers/MailerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class MailerController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function getIndex()
+    {
+        return "df";
+    }
+}
---
+++ resources/views/inbox.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 09.07.2016
+ * Time: 0:00
+ */
