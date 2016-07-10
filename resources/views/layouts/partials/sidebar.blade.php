4dd029f73c3d1863a576aa4fb7950202bfffb78b
werder33 <andreywow90@hmail.com>
2016-07-10 16:15:01 +0300

    active menu

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -8,8 +8,12 @@
         <ul class="sidebar-menu">
             <li class="header">Почта</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('/') }}"> <span class="glyphicon glyphicon-save"> Входящие</span></a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-open"> Отправленные</span></a></li>
+            <li @if(Request::is('/')||Request::is('inbox*')) class='active' @endif>
+                <a href="{{ url('/') }}"> <span class="glyphicon glyphicon-save"> Входящие</span></a>
+            </li>
+            <li @if(Request::is('sent')||Request::is('letter*')) class='active' @endif>
+                <a href="{{url('/sent')}}"><span class="glyphicon glyphicon-open"> Отправленные</span></a>
+            </li>
 
         </ul><!-- /.sidebar-menu -->
     </section>
